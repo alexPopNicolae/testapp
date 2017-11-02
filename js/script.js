d5a7d902635752f6875a1718d2916dc805a68385
Alex Pop <alexpop3u@gmail.com>
2017-11-02 22:41:10 +0200

    finisehd the project responsive, slider and bodyscroll

--- js/script.js
+++ js/script.js
@@ -2,10 +2,36 @@ $(document).ready(function() {
    $("#menuButton").on('click', function(){
        $(".sidebar_menu_wrapper").toggleClass('opened');
        $(".main_wrapper").toggleClass('opened');
+       $('body').toggleClass('no_body_scroll');
    });
 
    $("#sideMenuCloseButton").on('click', function() {
     $(".sidebar_menu_wrapper").removeClass('opened');
     $(".main_wrapper").removeClass('opened');
+    $('body').removeClass('no_body_scroll');
    });
+
+   $("#nextButton").on('click', function() {
+       var elements = $('.slider_container > article');
+       var numberOfElements = $('.slider_container > article').length;
+        var first = elements.eq(0);
+        var last = elements.eq(length-1);
+        first.insertAfter(last);
+        elements.first().addClass("visible");
+        elements.first().next().addClass("visible");
+        elements.first().next().next().addClass("visible");
+
+   });
+
+   $("#prewButton").on('click', function() {
+        var elements = $('.slider_container > article');
+        var numberOfElements = $('.slider_container > article').length;
+        var first = elements.eq(0);
+        var last = elements.eq(length-1);
+        
+        last.insertBefore(first);
+        elements.first().addClass("visible");
+        elements.first().next().addClass("visible");
+        elements.first().next().next().addClass("visible");
+    });
 });
